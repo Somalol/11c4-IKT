847c7dead7df10abf16256a1df2cfd3c5e7b396b
Farever <nagylajosdominik@gmail.com>
2022-11-15 09:58:52 +0100

    átírtam Classekre a bekérést is

--- rent.py
+++ rent.py
@@ -1,33 +1,28 @@
+from bookClass import Book
+
 booksList = []
 def fileRead():
     with open("file.txt", 'r', encoding = 'utf-8') as f:
-        actual = {}
         for line in f:
             title,writer,pages,date,rented = line.strip().split(";")
-            actual = {
-                "Title": title,
-                "Writer" : writer,
-                "Pages" : pages,
-                "Date" : date,
-                "Status" :rented
-            }
+            actual = Book(title, writer, pages, date, rented)
             booksList.append(actual)
 fileRead()
 
 def bookRent(id):
     id = id-1
     choose = booksList[id]
-    if choose["Status"] == "True":
+    if choose.rented == "True":
         print("Már kölcsönözve")
     else:
-        choose["Status"] = "True"
+        choose.rented = "True"
         print("Könyv ki kölcsönözve")
         with open("file.txt", 'r', encoding = 'utf-8') as f:
             data = f.readlines()
             print(data)
 
         with open("file.txt", 'w' ,encoding='utf-8') as f:
-            data[id] = booksList[id]["Title"]+";"+booksList[id]["Writer"]+";"+booksList[id]["Pages"]+";"+booksList[id]["Date"]+";"+booksList[id]["Status"]+"\n"
+            data[id] = booksList[id].title+";"+booksList[id].writer+";"+booksList[id].pages+";"+booksList[id].date+";"+booksList[id].rented+"\n"
             f.writelines(data)
 
-bookRent(10)
+bookRent(4)
