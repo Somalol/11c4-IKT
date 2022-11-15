b3110cfeca4b4617b69d4d73b5d37a17436a2e1e
Somalol <tivsoma@gmail.com>
2022-11-15 10:19:09 +0100

    login és bookClass frissítése

--- login.py
+++ login.py
@@ -1,11 +1,13 @@
 from tkinter import *
-
+from bookClass import Book
+booksList = []
 
 root = Tk()
 root.title("Bejelentkezés")
 root.configure(bg = "black")
 
 lista = []
+booksList = []
 
 lbl_felh = Label(root, text = "Felhasználónév:", bg="black", fg="white")
 lbl_felh.grid(row = 1, column = 1)
@@ -27,6 +29,13 @@ with open("loginusers.txt", "r", encoding = "utf-8") as adatok:
             "jelszo" : str(sor[2]),
             "rang" : str(sor[3])
         })
+
+with open("file.txt", 'r', encoding = 'utf-8') as f:
+    for line in f:
+        title,writer,pages,date,rented = line.strip().split(";")
+        actual = Book(title, writer, pages, date, rented)
+        booksList.append(actual)
+
 
 benev = ""
 befhsz = ""
@@ -52,9 +61,35 @@ def login():
 def mainpage():
     mainpage = Toplevel()
     mainpage.title(str(benev) + ": " + str(betype))
-    mainpage.config(bg = "black")   
+    mainpage.config(bg = "black")
+
+    etr_bookselect = Entry(mainpage, bg = "lightblue", width = 30)
+    etr_bookselect.grid(row = 0, column = 0)
+
+    btn_kolcsonzes = Button(mainpage, text = "Könyv Kölcsönzése", bg = "black", fg = "white", command = lambda:bookRent(10))
+    btn_kolcsonzes.grid(row = 0, column = 1)   
+    
+    for i in range(len(booksList)):
+        TitleLabel = Label(mainpage, text=str(i+1) + ". " + actual.writer + ":" + actual.title)
+        TitleLabel.grid(row = i+1, column = 0)
 
     mainpage.mainloop()
+
+def bookRent(id):
+    id = id-1
+    choose = booksList[id]
+    if choose.rented == "True":
+        print("Már kölcsönözve")
+    else:
+        choose.rented = "True"
+        print("Könyv ki kölcsönözve")
+        with open("file.txt", 'r', encoding = 'utf-8') as f:
+            data = f.readlines()
+            print(data)
+
+        with open("file.txt", 'w' ,encoding='utf-8') as f:
+            data[id] = booksList[id].title+";"+booksList[id].writer+";"+booksList[id].pages+";"+booksList[id].date+";"+booksList[id].rented+"\n"
+            f.writelines(data)
 
 
 btn_login = Button(root, text="Bejelentkezés", bg = "black", fg = "white", command=lambda:login()).grid(row = 5, column = 1)
