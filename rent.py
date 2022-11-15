5e06eec168c706098c7f146e657c386c90189a09
Farever <nagylajosdominik@gmail.com>
2022-11-15 09:41:56 +0100

    Rent done

--- rent.py
+++ rent.py
@@ -1,4 +1,33 @@
-from tkinter import *
+booksList = []
+def fileRead():
+    with open("file.txt", 'r', encoding = 'utf-8') as f:
+        actual = {}
+        for line in f:
+            title,writer,pages,date,rented = line.strip().split(";")
+            actual = {
+                "Title": title,
+                "Writer" : writer,
+                "Pages" : pages,
+                "Date" : date,
+                "Status" :rented
+            }
+            booksList.append(actual)
+fileRead()
 
-def rentBook():
-    print("")
+def bookRent(id):
+    id = id-1
+    choose = booksList[id]
+    if choose["Status"] == "True":
+        print("Már kölcsönözve")
+    else:
+        choose["Status"] = "True"
+        print("Könyv ki kölcsönözve")
+        with open("file.txt", 'r', encoding = 'utf-8') as f:
+            data = f.readlines()
+            print(data)
+
+        with open("file.txt", 'w' ,encoding='utf-8') as f:
+            data[id] = booksList[id]["Title"]+";"+booksList[id]["Writer"]+";"+booksList[id]["Pages"]+";"+booksList[id]["Date"]+";"+booksList[id]["Status"]+"\n"
+            f.writelines(data)
+
+bookRent(10)
