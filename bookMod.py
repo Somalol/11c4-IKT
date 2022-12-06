dac82d435e8b1baef059192cfa24d6778adfd4cd
VargaMilan01 <vargamilan723@gmail.com>
2022-12-06 09:21:12 +0100

    wip2.0

--- bookMod.py
+++ bookMod.py
@@ -16,35 +16,10 @@ def fileRead():
             booksList.append(actual)
 fileRead()
 
-def showBooks():
-    listBox.delete(*listBox.get_children())
-    for i in range(len(booksList)):
-        choosen = booksList[i]
-        listBox.insert("", "end", values=(i+1 ,choosen.title, choosen.writer, choosen.pages, choosen.date, choosen.rented))
-
-def bookSearch(search):
-    index = 1
-    listBox.delete(*listBox.get_children())
-
-    for i in booksList:
-        if i.title == str(search):
-            listBox.insert("", "end", values=(index ,i.title, i.writer, i.pages, i.date, i.rented))
-            index += 1
-        else:
-            if i.writer == search:
-                listBox.insert("", "end", values=(index ,i.title, i.writer, i.pages, i.date, i.rented))
-                index += 1
-            elif i.rented == search:
-                listBox.insert("", "end", values=(index,i.title, i.writer, i.pages, i.date, i.rented))
-                index += 1
-
-    if index == 1 :
-        messagebox.showerror(title = "Error", message = "Nincs találat")
-        showBooks()
 
 def modify():
 
-    mod = Toplevel(root)
+    mod = Tk()
 
     mod.title("Adatmódosítás")
 
@@ -82,12 +57,10 @@ def modify():
         valt = Toplevel(mod)
         id = int(c.get())
         id2 = b
-        print(id)
-        print(id2)
         id = id-1
         id2 = id2
         choose = booksList[id]
-        with open("testdata.txt", 'r', encoding = 'utf-8') as f:
+        with open("books.txt", 'r', encoding = 'utf-8') as f:
             data = f.readlines()
          
         choose = booksList[id]
@@ -98,30 +71,30 @@ def modify():
         def done():
             if id2 == 1:
                 choose.title = xa.get()
-                with open("testdata.txt", 'w' ,encoding='utf-8') as f:
+                with open("books.txt", 'w' ,encoding='utf-8') as f:
                     data[id] = booksList[id].title+";"+booksList[id].publisher+";"+booksList[id].writer+";"+booksList[id].pages+";"+booksList[id].date+";"+booksList[id].rented+"\n"
                     f.writelines(data)
             if id2 == 3:
                 choose.writer = xa.get() 
-                with open("testdata.txt", 'w' ,encoding='utf-8') as f:
+                with open("books.txt", 'w' ,encoding='utf-8') as f:
                     data[id] = booksList[id].title+";"+booksList[id].publisher+";"+booksList[id].writer+";"+booksList[id].pages+";"+booksList[id].date+";"+booksList[id].rented+"\n"
                     f.writelines(data)
             if id2 == 4: 
                 choose.pages = xa.get()
-                with open("testdata.txt", 'w' ,encoding='utf-8') as f:
+                with open("books.txt", 'w' ,encoding='utf-8') as f:
                     data[id] = booksList[id].title+";"+booksList[id].publisher+";"+booksList[id].writer+";"+booksList[id].pages+";"+booksList[id].date+";"+booksList[id].rented+"\n"
                     f.writelines(data)
             if id2 == 2:
                 choose.publisher = xa.get()
-                with open("testdata.txt", 'w' ,encoding='utf-8') as f:
+                with open("books.txt", 'w' ,encoding='utf-8') as f:
                     data[id] = booksList[id].title+";"+booksList[id].publisher+";"+booksList[id].writer+";"+booksList[id].pages+";"+booksList[id].date+";"+booksList[id].rented+"\n"
                     f.writelines(data)
             if id2 == 5:
                 choose.date = xa.get()
-                with open("testdata.txt", 'w' ,encoding='utf-8') as f:
+                with open("books.txt", 'w' ,encoding='utf-8') as f:
                     data[id] = booksList[id].title+";"+booksList[id].publisher+";"+booksList[id].writer+";"+booksList[id].pages+";"+booksList[id].date+";"+booksList[id].rented+"\n"
                     f.writelines(data)
-            lblmo = Label(valt, text="Adat megváltoztatva")
+            lblmo = messagebox.showinfo(title="Adatmódosítás", message="Adat megváltoztatva",)
             lblmo.pack()
             btn_mod = Button(valt, text="Bezárás", command= lambda: valt.destroy)
             btn_mod.pack()
@@ -134,24 +107,3 @@ def modify():
 
 
 
-
-
-root = Tk()
-#Table
-cols = ('Position', 'Cím','Író', 'Oldalak', 'Megjelenés éve' ,'Kölcsönzött-e')
-listBox = ttk.Treeview(root, columns=cols, show='headings')
-
-for col in cols:
-    listBox.heading(col, text=col)    
-listBox.grid(row=1, column=0, columnspan=2)
-
-showScores = tk.Button(root, text="Show books", width=15, command=showBooks).grid(row=4, column=0)
-closeButton = tk.Button(root, text="Close", width=15, command=exit).grid(row=4, column=1)
-#Table END
-
-#Search
-searchInput = Entry(root)
-searchInput.grid(row=4, column=2)
-searchBUtton = Button(root, text="Keresés", command=lambda: bookSearch(searchInput.get())).grid(row=4, column=3)
-
-root.mainloop()
